0c53e4b70aaa8ca032c8ddb0bce7113fbe9e9b2d
Christian <c.espinoza.code@gmail.com>
2023-07-20 11:02:39 -0500

    Logo img added

--- next.config.js
+++ next.config.js
@@ -2,6 +2,18 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
