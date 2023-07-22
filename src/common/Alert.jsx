5fd3d3ed76386e7bb67e60a17a4568bf42e8ec20
Christian <c.espinoza.code@gmail.com>
2023-07-21 21:17:39 -0500

    Styles for alert component modifed from gray to green

--- src/common/Alert.jsx
+++ src/common/Alert.jsx
@@ -1,4 +1,5 @@
 import { XCircleIcon } from '@heroicons/react/24/solid';
+import { CheckCircleIcon } from '@heroicons/react/24/outline';
 
 function Alert({ alert, handleClose }) {
   if (alert && alert?.autoClose) {
@@ -9,14 +10,27 @@ function Alert({ alert, handleClose }) {
 
   return (
     <>
-      {alert?.active && (
-        <div x-data className="bg-indigo-100 p-5 w-full rounded mb-8">
+      {/* {alert?.active && (
+        <div x-data className="bg-green-400 h- p-5 w-full rounded mb-8">
           <div className="flex space-x-3">
             <div className="flex-1 leading-tight text-sm text-black font-medium">{alert.message}</div>
             <button type="button">
               <XCircleIcon className="w-6 h-6 text-gray-600" onClick={handleClose} />
             </button>
           </div>
+        </div>
+      )} */}
+      {alert?.active && (
+        <div x-data className="bg-green-200 h-10 p-5 w-full rounded mb-8 flex justify-between">
+          <div className="leading-tight text-sm text-black font-medium flex items-center">
+            <span>
+              <CheckCircleIcon className="bg-red-500 w-8 h-8" />
+            </span>
+            <p className="w-[64px] text-center">{alert.message}</p>
+          </div>
+          <button className="" type="button">
+            <XCircleIcon className="w-6 h-6 text-gray-600" onClick={handleClose} />
+          </button>
         </div>
       )}
     </>
--- src/common/MyModal.jsx
+++ src/common/MyModal.jsx
@@ -5,9 +5,9 @@ function MyModal({ open, setOpen, children }) {
   return (
     <Dialog open={open} onClose={() => setOpen(false)} className="relative z-50 inset-0">
       <div className="fixed inset-0 bg-black/30">
-        <div className="fixed inset-0 flex items-center justify-center p-4">
+        <div className="fixed inset-0 flex items-center p-4">
           <Dialog.Panel className="mx-auto max-w-sm bg-white rounded-lg">
-            <div className=" px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse rounded-t-lg">
+            <div className="px-4 py-3 sm:px-6 sm:flex sm:justify-end rounded-t-lg">
               <XCircleIcon className="h-6 w-6 text-gray-400 cursor-pointer" aria-hidden="true" onClick={() => setOpen(false)} />
             </div>
             {children}
