c0d4a8d9cf002ec28d239a7500225a8e2402a1e1
Christian <c.espinoza.code@gmail.com>
2023-07-23 08:44:10 -0500

    nextjs document customized: color for body implemented

---
+++ src/pages/_document.js
@@ -0,0 +1,13 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <body className="bg-white dark:bg-[#3b3b3b]">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
