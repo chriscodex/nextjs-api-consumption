648717658e1a6c824fb9c648b408fa025c01d1cd
Christian <c.espinoza.code@gmail.com>
2023-07-23 08:36:28 -0500

    colors for Dark mode added

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,10 @@ module.exports = {
   theme: {
     colors: {
       ...colors,
+      dmBlack: '#3b3b3b',
+      dmWhite: '#F4EEE0',
     },
   },
+  darkMode: 'class',
   plugins: [require('@tailwindcss/forms')],
 };
