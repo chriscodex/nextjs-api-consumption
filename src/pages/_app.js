f3525294c0482f365ce1848461cebcc6183d0b5d
Christian <c.espinoza.code@gmail.com>
2023-07-23 09:25:59 -0500

    Update product logic implemented in services

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,14 +1,11 @@
 import { ProviderAuth } from 'hooks/useAuth';
-import MainLayout from '@layout/MainLayout';
 import '@styles/tailwind.css';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <ProviderAuth>
-        <MainLayout>
-          <Component {...pageProps} />
-        </MainLayout>
+        <Component {...pageProps} />
       </ProviderAuth>
     </>
   );
