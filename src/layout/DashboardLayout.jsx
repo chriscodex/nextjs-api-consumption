85c7ea06e8d914f458e8e696508df8f02a120746
Christian <c.espinoza.code@gmail.com>
2023-07-23 08:42:44 -0500

    custom layout created fro dashboard page

---
+++ src/layout/DashboardLayout.jsx
@@ -0,0 +1,11 @@
+export default function DashboardLayout({ children }) {
+  return (
+    <div>
+      <div className="min-h-screen w-full bg-black">
+        <main>
+          <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">{children}</div>
+        </main>
+      </div>
+    </div>
+  );
+}
