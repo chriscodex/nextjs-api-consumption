a003592f6a9ca237ad48ea47fd8ea0590456fa1b
Christian <c.espinoza.code@gmail.com>
2023-07-23 08:39:49 -0500

    context for all the app created

---
+++ src/context/appContext.js
@@ -0,0 +1,11 @@
+import { createContext, useState } from 'react';
+
+const AppContext = createContext();
+
+const AppContextProvider = ({ children }) => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  return <AppContext.Provider value={{ darkMode, setDarkMode }}>{children}</AppContext.Provider>;
+};
+
+export { AppContext, AppContextProvider };
