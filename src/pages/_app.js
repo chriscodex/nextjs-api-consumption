55d25fc9c9deae3b1a65dfe32717c987150d3d12
Christian <c.espinoza.code@gmail.com>
2023-07-24 08:45:58 -0500

    appContext & shoppingCart context injected in _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,18 @@
 import { ProviderAuth } from 'hooks/useAuth';
+import { ShoppingCartProvider } from '@hooks/useShoppingCart';
+import { AppContextProvider } from 'context/appContext';
 import '@styles/tailwind.css';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <ProviderAuth>
-        <Component {...pageProps} />
-      </ProviderAuth>
+      <AppContextProvider>
+        <ShoppingCartProvider>
+          <ProviderAuth>
+            <Component {...pageProps} />
+          </ProviderAuth>
+        </ShoppingCartProvider>
+      </AppContextProvider>
     </>
   );
 }
