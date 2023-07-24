22452113c9b10eee08d10f78e84ffc1c507a71ea
Christian <c.espinoza.code@gmail.com>
2023-07-24 08:48:08 -0500

    headerOptionsHandler & darkmode implemented in Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,16 @@
 /* This example requires Tailwind CSS v2.0+ */
-import { Fragment, useContext, useEffect } from 'react';
+import { Fragment, useContext, useEffect, useState } from 'react';
 import Cookies from 'js-cookie';
 import { Disclosure, Menu, Transition } from '@headlessui/react';
 import { BellIcon, Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
 import { AuthContext } from '@hooks/useAuth';
 import Image from 'next/image';
-import ccLogo from '../../public/chriscodex-isotipo-light.svg';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-
-const navigation = [
-  { name: 'Dashboard', href: '/dashboard', current: true },
-  { name: 'Productos', href: '/dashboard/products/', current: false },
-];
-const userNavigation = [{ name: 'Sign out', href: '/' }];
+import { ShoppingCartContext } from '@hooks/useShoppingCart';
+import { AppContext } from 'context/appContext';
+import logoDark from '../../public/logotipo-dark.svg';
+import logoLight from '../../public/logotipo-light.svg';
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ');
@@ -21,6 +18,40 @@ function classNames(...classes) {
 
 export default function Header() {
   const router = useRouter();
+
+  const [headerSelection, setHeaderSelection] = useState('Dashboard');
+
+  useEffect(() => {
+    if (router.pathname === '/dashboard') {
+      setHeaderSelection('Dashboard');
+    } else if (router.pathname === '/dashboard/products' || router.pathname === '/dashboard/products/add-product' || router.pathname === '/dashboard/products/edit/[id]') {
+      setHeaderSelection('Products');
+    }
+  }, []);
+
+  const { navDashboard, setNavDashboard, navProducts, setNavProducts } = useContext(ShoppingCartContext);
+  const { darkMode, setDarkMode } = useContext(AppContext);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+    const htmlElement = document.querySelector('html');
+    htmlElement.classList.toggle('dark');
+  };
+
+  const onDashboardClick = () => {
+    setNavDashboard(true);
+    setNavProducts(false);
+  };
+
+  const onProductsClick = () => {
+    setNavProducts(true);
+    setNavDashboard(false);
+  };
+
+  const userNavigation = [
+    { name: 'Sign out', href: '/' },
+    { name: `Dark theme: ${darkMode ? 'On' : 'Off'}`, href: '/' },
+  ];
 
   const auth = useContext(AuthContext);
 
@@ -37,46 +68,92 @@ export default function Header() {
     }
   }, []);
 
+  const [dtButton, setDtButton] = useState('');
+
+  useEffect(() => {
+    if (darkMode) {
+      setDtButton('On');
+    } else {
+      setDtButton('Off');
+    }
+  }, [darkMode]);
+
   return (
     <>
-      <Disclosure as="nav" className="bg-gray-800">
+      <Disclosure as="nav" className="bg-gray-800 dark:bg-[#F4EEE0]">
         {({ open }) => (
           <>
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
               <div className="flex items-center justify-between h-16">
                 <div className="flex items-center">
                   <div className="flex-shrink-0">
-                    <Image width={12} height={12} className="h-12 w-12" src={ccLogo} alt="Workflow" />
+                    {darkMode ? (
+                      <a href="/">
+                        <Image width={12} height={12} className="h-12 w-12" src={logoLight} alt="Workflow" />
+                      </a>
+                    ) : (
+                      <a href="/">
+                        <Image width={12} height={12} className="h-12 w-12" src={logoDark} alt="Workflow" />
+                      </a>
+                    )}
                   </div>
                   <div className="hidden md:block">
                     <div className="ml-10 flex items-baseline space-x-4">
-                      {navigation.map((item) => (
-                        <Link
-                          key={item.name}
-                          href={item.href}
-                          className={classNames(item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'px-3 py-2 rounded-md text-sm font-medium')}
-                          aria-current={item.current ? 'page' : undefined}
+                      <Link
+                        key="dashboard"
+                        href="/dashboard"
+                        className={classNames(
+                          headerSelection === 'Dashboard'
+                            ? 'bg-gray-900 text-white dark:text-dmBlack dark:bg-white  hover:text-white'
+                            : 'text-gray-300 hover:bg-gray-700 dark:text-dmBlack dark:hover:bg-white',
+                          'px-3 py-2 rounded-md text-sm font-medium '
+                        )}
+                        aria-current={navDashboard ? 'page' : undefined}
+                      >
+                        <button
+                          onClick={() => {
+                            onDashboardClick();
+                          }}
                         >
-                          {item.name}
-                        </Link>
-                      ))}
+                          Dashboard
+                        </button>
+                      </Link>
+                      <Link
+                        key="products"
+                        href="/dashboard/products/"
+                        className={classNames(
+                          headerSelection === 'Products'
+                            ? 'bg-gray-900 text-white dark:text-dmBlack dark:bg-white  hover:text-white'
+                            : 'text-gray-300 hover:bg-gray-700 dark:text-dmBlack dark:hover:bg-white',
+                          'px-3 py-2 rounded-md text-sm font-medium '
+                        )}
+                        aria-current={navProducts ? 'page' : undefined}
+                      >
+                        <button
+                          onClick={() => {
+                            onProductsClick();
+                          }}
+                        >
+                          Products
+                        </button>
+                      </Link>
                     </div>
                   </div>
                 </div>
                 <div className="hidden md:block">
                   <div className="ml-4 flex items-center md:ml-6">
-                    <button
+                    {/* <button
                       type="button"
-                      className="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+                      className="bg-gray-800 dark:bg-[#6D5D6E] p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
                     >
                       <span className="sr-only">View notifications</span>
-                      <BellIcon className="h-6 w-6" aria-hidden="true" />
-                    </button>
+                      <BellIcon className="h-6 w-6 dark:text-black" aria-hidden="true" />
+                    </button> */}
 
                     {/* Profile dropdown */}
                     <Menu as="div" className="ml-3 relative">
                       <div>
-                        <Menu.Button className="max-w-xs bg-gray-800 rounded-full flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+                        <Menu.Button className="max-w-xs bg-gray-800 rounded-full flex items-center text-sm">
                           <span className="sr-only">Open user menu</span>
                           <Image width={8} height={8} className="h-8 w-8 rounded-full" src={userData.imageUrl} alt="" />
                         </Menu.Button>
@@ -90,8 +167,12 @@ export default function Header() {
                         leaveFrom="transform opacity-100 scale-100"
                         leaveTo="transform opacity-0 scale-95"
                       >
-                        <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                          <button onClick={() => auth.logout()} className="block px-4 py-2 text-sm text-gray-700">
+                        <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black dark:ring-dmWhite dark:ring-opacity-5 ring-opacity-5 focus:outline-none dark:bg-dmBlack">
+                          <button onClick={() => toggleDarkMode()} className="block px-4 py-2 text-sm text-gray-700 dark:text-dmWhite">
+                            {`Dark theme: ${dtButton}`}
+                          </button>
+
+                          <button onClick={() => auth.logout()} className="block px-4 py-2 text-sm text-gray-700 dark:text-dmWhite">
                             Logout
                           </button>
                         </Menu.Items>
@@ -110,19 +191,6 @@ export default function Header() {
             </div>
 
             <Disclosure.Panel className="md:hidden">
-              <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-                {navigation.map((item) => (
-                  <Disclosure.Button
-                    key={item.name}
-                    as="a"
-                    href={item.href}
-                    className={classNames(item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'block px-3 py-2 rounded-md text-base font-medium')}
-                    aria-current={item.current ? 'page' : undefined}
-                  >
-                    {item.name}
-                  </Disclosure.Button>
-                ))}
-              </div>
               <div className="pt-4 pb-3 border-t border-gray-700">
                 <div className="flex items-center px-5">
                   <div className="flex-shrink-0">
